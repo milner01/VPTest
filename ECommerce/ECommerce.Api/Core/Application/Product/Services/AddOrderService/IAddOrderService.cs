4369611bbf3a05a026a8fded6cd5a0703c5604b7
chris milner <chrismilner445@gmail.com>
2023-12-01 16:16:04 +0000

    removed unrequired code

--- ECommerce/ECommerce.Api/Core/Application/Product/Services/AddOrderService/IAddOrderService.cs
+++
@@ -1,8 +0,0 @@
-﻿using ECommerce.Api.Core.Domain.Entities;
-
-namespace ECommerce.Api.Core.Application.Product.Services.AddOrderService;
-
-public interface IAddOrderService
-{
-    void AddOrder(Order order, CancellationToken cancellationToken);
-}
